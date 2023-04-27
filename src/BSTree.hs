a0c17aa75d7574deec621927694ee35e095a347e
Luke <luke.sutton.work@gmail.com>
2023-04-28 00:33:43 +0100

    remove if

--- src/BSTree.hs
+++ src/BSTree.hs
@@ -3,6 +3,8 @@ module BSTree (
     insertIntoBSTree,
     lookupBSTree,
     listBSTreeVals,
+    removeFromBSTree,
+    removeEntriesIf,
     BSTree(..)
 ) where
 
@@ -38,3 +40,26 @@ listBSTreeVals (Node k v left right) = sortedLeft ++ [(k, v)] ++ sortedRight
     sortedLeft = listBSTreeVals left
     sortedRight = listBSTreeVals right
 
+removeFromBSTree :: Ord k => k -> BSTree k v -> BSTree k v
+removeFromBSTree _ Empty = Empty
+removeFromBSTree key (Node k v left right)
+    | key == k = removeNode (Node k v left right)
+    | key < k  = Node k v (removeFromBSTree key left) right
+    | otherwise = Node k v left (removeFromBSTree key right)
+  where
+    removeNode :: Ord k => BSTree k v -> BSTree k v
+    removeNode (Node _ _ Empty r) = r
+    removeNode (Node _ _ l Empty) = l
+    removeNode (Node _ _ l r) = Node k' v' l (removeFromBSTree k' r)
+      where
+        (k', v') = findMin r
+        findMin :: Ord k => BSTree k v -> (k, v)
+        findMin (Node k v Empty _) = (k, v)
+        findMin (Node _ _ l _) = findMin l
+
+
+removeEntriesIf :: Ord k => (k -> v -> Bool) -> BSTree k v -> BSTree k v
+removeEntriesIf _ Empty = Empty
+removeEntriesIf p (Node k v left right)
+    | p k v = removeEntriesIf p (removeFromBSTree k (Node k v left right))
+    | otherwise = Node k v (removeEntriesIf p left) (removeEntriesIf p right)
