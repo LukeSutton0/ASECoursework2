504c3362da571de8d8fee8fc522ec655f09e3658
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-04-27 14:55:39 +0100

    Changing to unique vals for Gen

--- test/BSTTests.hs
+++ test/BSTTests.hs
@@ -1,4 +1,5 @@
 module BSTTests (bSTreeMain) where
+
 import Test.HUnit
 import Test.QuickCheck
 import BSTree
@@ -22,25 +23,37 @@ testInsertIntoBSTree = TestCase $ do
       (Node 1 "My Value for the inserted node" Empty Empty) aTreeWithANode
 
 -- QuickCheck tests for `binary Search Tree Tests`
--- Generate binary search trees
-genBSTree :: Gen (BSTree Int String)
-genBSTree = do
-  frequency [
-      (1, return Empty),
-      (4, do 
-          k <- arbitrary
-          v <- arbitrary
-          l <- genBSTree
-          r <- genBSTree
-          return $ Node k v l r)]
+-- Generate binary search trees with at most `n` nodes
+genBSTree :: Int -> Gen (BSTree Int String)
+genBSTree n = genBSTree' n []
+  where
+    genBSTree' :: Int -> [Int] -> Gen (BSTree Int String)
+    genBSTree' 0 _ = return Empty
+    genBSTree' n usedKeys = frequency [
+        (1, return Empty),
+        (6, do 
+            k <- choose (1, n)
+            k <- uniqueKey k usedKeys
+            v <- resize 5 $ listOf $ elements ['a'..'z']
+            let newUsedKeys = k : usedKeys
+            l <- genBSTree' (n `div` 2) newUsedKeys
+            r <- genBSTree' (n `div` 2) newUsedKeys
+            return $ Node k v l r)]
+
+    uniqueKey :: Int -> [Int] -> Gen Int --make sure key is unique
+    uniqueKey k usedKeys
+      | k `elem` usedKeys = do
+          newK <- choose (1, length usedKeys)
+          uniqueKey newK usedKeys
+      | otherwise = return k
 
 testInsertIntoBSTreeExists :: Property
 testInsertIntoBSTreeExists =
-  forAll genBSTree $ \t -> --random tree
-    forAll arbitrary $ \k -> --random key
-      forAll arbitrary $ \v -> --random value
-        let newT = insertIntoBSTree k v t --all put together
-        in existsInBSTree k newT --testing function vs newT
+  forAll (genBSTree 100) $ \t -> -- random tree with at most 100 nodes
+    forAll arbitrary $ \k -> -- random key
+      forAll arbitrary $ \v -> -- random value
+        let newT = insertIntoBSTree k v t -- insert key-value pair into tree
+        in existsInBSTree k newT -- check if key exists in new tree
 
 existsInBSTree :: Ord k => k -> BSTree k v -> Bool
 existsInBSTree _ Empty = False
@@ -51,22 +64,25 @@ existsInBSTree k (Node k' _ l r)
 
 testLookupBSTree :: Property
 testLookupBSTree =
-  forAll genBSTree $ \t ->
+  forAll (genBSTree 10) $ \t -> -- random tree with at most 100 nodes
     case t of
       Empty -> property True -- skip if tree is empty
-      _ -> --for any other tree
-        let kvs = getKeyValuePairs t
-        in forAll (elements kvs) $ \(k, v) ->
-             let mval = lookupBSTree k t
-             in counterexample (show (k, v, mval)) (mval == Just v)
+      _ ->
+        forAll arbitrary $ \k -> -- generates random key
+          let expected = lookup k (getKeyValuePairs t) --tries to find the key in the tree if not found returns nothing
+              actual = lookupBSTree k t --
+          in counterexample (show (k, expected, actual)) $
+             (existsInBSTree k t && actual == expected) || (not (existsInBSTree k t) && actual == Nothing)
 
 getKeyValuePairs :: BSTree k v -> [(k, v)]
 getKeyValuePairs Empty = []
 getKeyValuePairs (Node k v l r) = getKeyValuePairs l ++ [(k, v)] ++ getKeyValuePairs r
 
 
+
 bSTreeMain :: IO ()
 bSTreeMain = do
-  _ <- runTestTT binarySearchTreeTests
+  -- _ <- runTestTT binarySearchTreeTests --run all HUnit tests
   quickCheck testInsertIntoBSTreeExists
+  --quickCheck testLookupBSTree
   quickCheck testLookupBSTree
